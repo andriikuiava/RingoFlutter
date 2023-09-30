b5ed6ad8a621c7fe7b53fa8d920e2fdd5402ea2d
andriikuiava <125557951+andriikuiava@users.noreply.github.com>
2023-09-30 16:32:41 +0300

    social sharing

--- lib/Event/StickerPageToShare.dart
+++ lib/Event/StickerPageToShare.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:ringoflutter/Classes/EventClass.dart';
+import 'package:ringoflutter/UI/Functions/Formats.dart';
 import 'package:screenshot/screenshot.dart';
 import 'package:ringoflutter/api_endpoints.dart';
+import 'package:adaptive_action_sheet/adaptive_action_sheet.dart';
+import 'package:share_plus/share_plus.dart';
 
 class StickerPageToShare extends StatefulWidget {
   final EventFull event;
@@ -15,13 +20,20 @@ class StickerPageToShare extends StatefulWidget {
 }
 
 class _StickerPageToShareState extends State<StickerPageToShare> {
+  bool isOtherLoading = false;
   @override
   Widget build(BuildContext context) {
     var currentTheme = Theme.of(context);
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
         backgroundColor: currentTheme.scaffoldBackgroundColor,
-        middle: const Text("Share event"),
+        middle: Text(
+            "Share event",
+            style: TextStyle(
+              color: currentTheme.colorScheme.primary,
+              fontSize: 18,
+            ),
+        ),
         leading: GestureDetector(
           onTap: () {
             Navigator.pop(context);
@@ -49,14 +61,14 @@ class _StickerPageToShareState extends State<StickerPageToShare> {
                         color: currentTheme.primaryColor,
                         child: ListTile(
                           leading: (widget.event.host.profilePictureId != null)
-                          ? CircleAvatar(
-                              radius: 24,
-                              backgroundColor: currentTheme.colorScheme.background,
-                              backgroundImage: NetworkImage(
-                                "${ApiEndpoints.GET_PHOTO}/${widget.event.host.profilePictureId}",
-                              ),
+                              ? CircleAvatar(
+                            radius: 24,
+                            backgroundColor: currentTheme.colorScheme.background,
+                            backgroundImage: NetworkImage(
+                              "${ApiEndpoints.GET_PHOTO}/${widget.event.host.profilePictureId}",
+                            ),
                           )
-                          : null,
+                              : null,
                           title: Text(
                             widget.event.name,
                             style: TextStyle(
@@ -86,9 +98,19 @@ class _StickerPageToShareState extends State<StickerPageToShare> {
                           padding: const EdgeInsets.all(14.0),
                           child: Column(
                             children: [
-                              SizedBox(
+                              Container(
                                 width: MediaQuery.of(context).size.width - 56,
                                 height: MediaQuery.of(context).size.width - 56,
+                                decoration: BoxDecoration(
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black.withOpacity(0.2),
+                                      spreadRadius: 2,
+                                      blurRadius: 10,
+                                      offset: Offset(0, 2),
+                                    ),
+                                  ],
+                                ),
                                 child: ClipRRect(
                                   borderRadius: BorderRadius.circular(20),
                                   child: Image.network(
@@ -102,13 +124,73 @@ class _StickerPageToShareState extends State<StickerPageToShare> {
                                 crossAxisAlignment: CrossAxisAlignment.center,
                                 children: [
                                   Icon(
-                                    CupertinoIcons.calendar,
+                                    CupertinoIcons.calendar_today,
                                     color: currentTheme.primaryColor,
                                     size: 24,
                                   ),
-                                  const SizedBox(width: 4,),
+                                  const SizedBox(width: 8,),
                                   Text(
-                                    widget.event.startTime.toString().substring(0, 10),
+                                    startTimeFromTimestamp(widget.event.startTime!, widget.event.endTime),
+                                    style: TextStyle(
+                                      decoration: TextDecoration.none,
+                                      color: currentTheme.primaryColor,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.normal,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 8,),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    CupertinoIcons.map_pin,
+                                    color: currentTheme.primaryColor,
+                                    size: 24,
+                                  ),
+                                  const SizedBox(width: 8,),
+                                  Text(
+                                    widget.event.address!,
+                                    style: TextStyle(
+                                      decoration: TextDecoration.none,
+                                      color: currentTheme.primaryColor,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.normal,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 8,),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    CupertinoIcons.money_dollar_circle,
+                                    color: currentTheme.primaryColor,
+                                    size: 24,
+                                  ),
+                                  const SizedBox(width: 8,),
+                                  Text(
+                                    (widget.event.price != null)
+                                    ? (widget.event.price == 0)
+                                      ? "Free"
+                                      : "from ${widget.event.currency!.symbol}${widget.event.price!.toStringAsFixed(2)}"
+                                    : constructPrice(widget.event),
+                                    style: TextStyle(
+                                      decoration: TextDecoration.none,
+                                      color: currentTheme.primaryColor,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.normal,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 8,),
+                              Row(
+                                children: [
+                                  Text(
+                                    "www.ringo-events.com",
                                     style: TextStyle(
                                       decoration: TextDecoration.none,
                                       color: currentTheme.primaryColor,
@@ -123,6 +205,71 @@ class _StickerPageToShareState extends State<StickerPageToShare> {
                         ),
                       ),
                     ),
+                    const SizedBox(height: 12,),
+                    Container(
+                      width: MediaQuery.of(context).size.width - 20,
+                      child: CupertinoButton(
+                        color: currentTheme.colorScheme.background,
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Spacer(),
+                            Icon(
+                              CupertinoIcons.share,
+                              color: currentTheme.colorScheme.primary,
+                              size: 24,
+                            ),
+                            const SizedBox(width: 8,),
+                            Text(
+                              "Share",
+                              style: TextStyle(
+                                color: currentTheme.colorScheme.primary,
+                                fontSize: 18,
+                              ),
+                            ),
+                            Spacer(),
+                          ],
+                        ),
+                        onPressed: () {
+                          showAdaptiveActionSheet(
+                            context: context,
+                            title: const Text('Share event'),
+                            androidBorderRadius: 30,
+                            actions: <BottomSheetAction>[
+                              BottomSheetAction(title: const Text('Add to Instagram Story'), onPressed: (context) {}),
+                              BottomSheetAction(title: const Text('Add to Facebook Story'), onPressed: (context) {}),
+                              BottomSheetAction(title: (isOtherLoading) ? CupertinoActivityIndicator(color: currentTheme.primaryColor,) : Text('Other options'), onPressed: (context) async {
+                                setState(() {
+                                  isOtherLoading = true;
+                                });
+                                var file = await DefaultCacheManager().getSingleFile(
+                                    "${ApiEndpoints.GET_PHOTO}/${widget.event.mainPhoto.mediumQualityId}");
+                                await Share.shareXFiles(
+                                  [
+                                    XFile(
+                                      file.path,
+                                    ),
+                                  ],
+                                  text:
+                                      "Check out ${widget.event.name} by ${widget.event.host.name} on Ringo!\nhttps://ringo-events.com/event/${widget.event.id}",
+                                );
+                                setState(() {
+                                  isOtherLoading = false;
+                                });
+                              }),
+                            ],
+                            cancelAction: CancelAction(
+                                title: Text('Cancel',
+                                  style: TextStyle(
+                                    color: Colors.red,
+                                    fontSize: 18,
+                                  )
+                                ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
                   ],
                 )
               ),
@@ -133,5 +280,6 @@ class _StickerPageToShareState extends State<StickerPageToShare> {
     );
   }
 }
+
 
 
