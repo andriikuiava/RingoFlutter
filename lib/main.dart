a7881ad000d060ab2979194df123285c0f4199d9
andriikuiava <125557951+andriikuiava@users.noreply.github.com>
2023-10-01 11:12:54 +0300

    payments imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:ringoflutter/Security/checkIsLoggedIn.dart';
 
 import 'UI/Themes.dart';
 
 void main() {
+  Stripe.publishableKey = "pk_test_51N6bKSCkVoLPfCXzfYqskbTCIxe6Bt2B8aYLiuZAUEQaPB3TGmAekPo3rJyOgaU0IXYFWNcHfAxh9L4NdSjUgY0700tQqdVwxm";
   runApp(
     MaterialApp(
       navigatorKey: App.materialKey,
